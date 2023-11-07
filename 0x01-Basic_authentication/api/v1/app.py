9d9eecd381954a699ac163b63ff423cc584eff11
pitzee <pitzee421@gmail.com>
2023-11-07 20:34:49 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,7 +25,14 @@ def not_found(error) -> str:
 def unauthorized(error) -> str:
     """ unauthorized handler
     """
-    return jsonify({"error": "unauthorized"}), 401
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ forbidden handler
+    """
+    return jsonify({"error": " Forbidden"}), 403
 
 
 if __name__ == "__main__":
