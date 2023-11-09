b07453720bf0dab6e4090d06d66520d8c5f5d34e
pitzee <pitzee421@gmail.com>
2023-11-09 11:00:55 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,12 +39,14 @@ def filter():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(path, excluded_paths) is False:
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)
