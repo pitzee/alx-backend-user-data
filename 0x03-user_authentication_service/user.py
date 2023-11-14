5b11ef6c4c16e4e8ea6798007366280155a89dbf
pitzee <pitzee421@gmail.com>
2023-11-14 20:09:31 +0300

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,6 @@
 """ SQLAlchemy model named User """
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
