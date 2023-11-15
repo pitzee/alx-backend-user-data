21be8556c58682b7701acdc99a6ad0d20fc8df6b
pitzee <pitzee421@gmail.com>
2023-11-15 21:46:23 +0300

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" auth module  """
-
+"""auth module
+"""
 import bcrypt
 from db import DB
 from user import User
@@ -9,8 +9,8 @@ import uuid
 
 def _hash_password(password: str) -> bytes:
     """ returns a passwd hash """
-    hashed_pwd = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt(4))
-    return hashed_pwd
+    h_psw = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt(4))
+    return h_psw
 
 
 def _generate_uuid() -> str:
@@ -48,6 +48,7 @@ class Auth:
                 return True
             return False
         except Exception:
+            return False
 
     def create_session(self, email: str) -> str:
         """ creates a new session for user with email """
@@ -59,3 +60,39 @@ class Auth:
         except Exception:
             pass
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ gets user by session_id """
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
+
+    def destroy_session(self, user_id: str) -> User:
+        """ removes user's session_id """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ updates a users reset token field """
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ updates a users password """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except Exception:
+            raise ValueError
+        hashed_p = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_p,
+                             reset_token=None)
