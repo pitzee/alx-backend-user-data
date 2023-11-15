019872c1713077d702b687a5b7ba66f20832410f
pitzee <pitzee421@gmail.com>
2023-11-15 22:23:22 +0300

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,5 +56,15 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """profile route"""
+    sess_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(sess_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
